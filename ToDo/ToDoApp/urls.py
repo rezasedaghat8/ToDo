824ae6546da05f1078ec5ecd037d3d5a26a97a91
Saba <sa.kazemi09@gmail.com>
2025-04-10 11:06:26 +0330

    fixed errors and urls

--- ToDo/ToDoApp/urls.py
+++ ToDo/ToDoApp/urls.py
@@ -1,12 +1,10 @@
-
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
 
 router = DefaultRouter()
-router.register(r'tasks', TaskViewSet, basename='task')
+router.register(r'todos', TaskViewSet)
 
 urlpatterns = [
-    path('api/todos', include(router.urls)),
+    path('', include(router.urls))
 ]
